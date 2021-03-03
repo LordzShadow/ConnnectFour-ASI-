0ac86698b1c2c9946f7e8ab6d82c4ff408e86c94
LordzShadow <44734448+LordzShadow@users.noreply.github.com>
2021-03-03 14:49:28 +0200

    remove buld, .idea and .gradle

--- build/jpackage/Connnect Four/runtime/bin/Connnect Four.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set DIR="%~dp0"
-set JAVA_EXEC="%DIR:"=%\java"
-
-
-
-pushd %DIR% & %JAVA_EXEC% %CDS_JVM_OPTS% -Djdk.gtk.version=2 -p "%~dp0/../app" -m connnectfour/connnectfour.Main  %* & popd
